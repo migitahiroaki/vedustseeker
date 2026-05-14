f93ce5766eeafcf01f0ca7940146d96ffd59c136
Migita Hiroaki <migitahiroaki@gmail.com>
2026-05-14 22:52:27 +0900

    サブスクリプションフィルタ用に、ログをstringifyより前にした

--- src/seeker.ts
+++ src/seeker.ts
@@ -21,8 +21,8 @@ export const handler = async (
   console.debug("フィルタ条件", condition);
 
   const nfts = await fetchNftsWithCache(appEnv, condition);
+  console.info(nfts);
   const body = JSON.stringify(nfts);
-  console.info(body);
 
   return {
     statusCode: 200,
